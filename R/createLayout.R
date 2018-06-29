972df5fd45374d4b53e0b9919d840e9d18e34eae
Enrica Calura <enrica.calura@gmail.com>
2018-06-29 13:14:14 +0300

    The function showMOSpalette() to show the palette to the user.

--- R/createLayout.R
+++ R/createLayout.R
@@ -1,3 +1,23 @@
+#' Shows the MOSClip palette.
+#'
+#' This function shows the MOSClip palette. 
+#' Each omic should be coupled to a color panel, this match will be preserved in plots.
+#'
+#' @examples
+#' showMOSpalette()
+#' 
+#' @importFrom graphics axis title
+#'
+#' @export
+showMOSpalette <- function(){
+  plot(c(1:6,1:6,1:6,1:6), c(rep(1,6),rep(2,6), rep(3,6), rep(4,6)),
+       col=apply(MOSpaletteSchema,2,c), pch=19, cex=13, xlim=c(0.5,6.5) , 
+       ylim=c(0,4.8), xlab="", ylab="", axes=FALSE)
+  axis(3, at=1:6, labels=rownames(MOSpaletteSchema), las=1, 
+       cex.axis=0.8, lwd=0, pos=4.5, font=4)
+  title("MOSClip palette")
+}
+
 createLayout <- function(heatNumber=1, withTopAnno=TRUE, withSampleNames=TRUE, annotaionLegend=TRUE){
   body <- NULL
   for (i in seq_len(heatNumber)) {
